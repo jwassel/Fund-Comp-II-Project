6f0fe814712ba2a635a984dd25f513e132df2f11
Jason Martin Wassel <jwassel@nd.edu>
2013-03-20 20:35:52 -0400

    Enemy and Weapon Class Uploaded

---
+++ Enemy.h
@@ -0,0 +1,32 @@
+//This is the abstract class for our enemies
+#ifndef ENEMY_H
+#define ENEMY_H
+#include "SDL/SDL.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Enemy {
+public:
+	Enemy(int, int, int, int, int, string);
+	virtual void move()=0;
+	void spawn();
+	void die();
+	int attack();
+	int isColliding();
+	int getEnemyHealth();	
+	void getHit(int);
+	int isDead();
+private:
+	int xpos;
+	int ypos;
+	int health;
+	int power;
+	int speed;
+	SDL_Surface image;
+};
+
+#endif
+
