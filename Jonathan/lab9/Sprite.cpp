671387a2823b9b30641596ea1c8de6de8911c348
oliverlamb <olamb@nd.edu>
2013-04-01 16:02:33 -0400

    Added color keying to Sprite.cpp

--- Jonathan/lab9/Sprite.cpp
+++ Jonathan/lab9/Sprite.cpp
@@ -15,32 +15,34 @@ void Sprite::apply_surface(SDL_Surface* source, SDL_Surface* destination, SDL_Re
     SDL_BlitSurface(source, clip, destination, &shift);
 }
 
-SDL_Surface * Sprite::load_image(std::string filename, int r, int g, int b){ //takes in image of sprite
-    //The image that's loaded
-    SDL_Surface* loadedImage = NULL;
+SDL_Surface * Sprite::load_image(std::string filename){ //takes in image of sprite
+	//temporary storage for the image that's loaded
+	SDL_Surface* loadedImage = NULL;
 
-    //The optimized surface that will be used
-    SDL_Surface* optimizedImage = NULL;
+	//The optimized image that will be used
+	SDL_Surface* optimizedImage = NULL;
 
-    //Load the image
-    loadedImage = SDL_LoadBMP(filename.c_str());
+	//Load the iamge
+	loadedImage = IMG_Load(filename.c_str());
+	//this is 24 bit so not good idea to use this b/c causes slowdown
+	
+	//if nothing went wrong in loading the image
+	if(loadedImage!=NULL)
+	{
+		//create an optimized image in the same format as the screen (32 bit)
+		optimizedImage = SDL_DisplayFormat(loadedImage);
 
-    //If the image loaded
-   if(loadedImage != NULL){
-        //Create an optimized surface
-        optimizedImage = SDL_DisplayFormat(loadedImage);
+	//Free the old image
+	SDL_FreeSurface(loadedImage);
 
-        //Free the old surface
-        SDL_FreeSurface(loadedImage);
+		if(optimizedImage!=NULL)
+		{
+			//Map the color key
+			Uint32 colorkey = SDL_MapRGB( optimizedImage -> format,0xec,0x26,0x26);
 
-        //If the surface was optimized
-	if(optimizedImage != NULL){
-            //Color key surface
-	  int colorkey = SDL_MapRGB(optimizedImage->format, r, g, b);
-	  SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+			//Set all pizels of color R 0, G 0xFF, B 0xFF to be transparent
+			SDL_SetColorKey(optimizedImage,SDL_SRCCOLORKEY, colorkey);
+		}
 	}
-   }
-
-    //Return the optimized surface
-    return optimizedImage;
+		return optimizedImage;
 }
