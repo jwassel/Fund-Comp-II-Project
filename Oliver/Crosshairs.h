4760c8abef8bae765a573cd03c0e1ac18eec9fe3
oliverlamb <olamb@nd.edu>
2013-04-24 15:53:29 -0400

    Have crosshairs changing to red when hitting enemy and back to green after

--- Oliver/Crosshairs.h
+++ Oliver/Crosshairs.h
@@ -12,11 +12,15 @@ Crosshairs();
 void show(SDL_Surface*);
 SDL_Surface * load_image(string);
 void getMousePosition();
+void setImage(string);
+void setCrosshairsGreen(bool);
+bool isTheCrosshairsGreen();
 
 private:
 SDL_Surface *image;
 int xposition;
 int yposition;
+bool isCrosshairsGreen;
 
 };
 #endif
--- Oliver/Weapon.cpp
+++ Oliver/Weapon.cpp
@@ -6,10 +6,11 @@
 #include "Weapon.h"
 #include "Enemy.h"
 #include <iostream>
+#include <string>
 using namespace std;
-Weapon::Weapon(string filename,string explosionName, int Price, int AmmoPrice, int Damage, int FireRate, int x, int y,int expsize, int max, int current, int maxclip, int currentclip):Item(x,y,Price,filename){
+Weapon::Weapon(string filename,string explosionName, string Name,int Price, int AmmoPrice, int Damage, int FireRate, int x, int y,int expsize, int max, int current, int maxclip, int currentclip):Item(x,y,Price,filename){
 
-
+name=Name;
 ammoPrice = AmmoPrice;
 damage = Damage;
 fireRate = fireRate;
@@ -19,6 +20,7 @@ currentAmmo=current;
 maxClipAmmo=maxclip;
 currentClipAmmo=currentclip;
 reloadTime=RELOAD_TIME;
+crosshairsReloadTime=30;
 
 
 sprite = load_image(filename.c_str());
@@ -26,19 +28,51 @@ explosionImage = load_image(explosionName.c_str());
 
 }
 
+string Weapon::getName(){
+return name;
+}
+
 bool Weapon::addToCurrentAmmo(int m)
 {
-	if(currentAmmo==maxAmmo)
-	{
+
+	if(currentAmmo+currentClipAmmo==maxAmmo+maxClipAmmo){
 		return false;
 	}
-	if(currentAmmo+m>maxAmmo)
-	{
-		currentAmmo = maxAmmo;
+	if(currentAmmo+currentClipAmmo+m>=maxAmmo+maxClipAmmo) {
+
+		currentAmmo=maxAmmo;
+		currentClipAmmo=maxClipAmmo;
+	
+		return true;
 	}
-	else
-		currentAmmo += m;
+	if(currentAmmo+m<maxAmmo){
+			
+		currentAmmo+=m;
+		return true;	
+	}
+	if(currentAmmo<maxAmmo){
+		
+		currentAmmo=maxAmmo;
+		return true;
+	}
+	if(currentClipAmmo<maxClipAmmo){
+			currentClipAmmo+=m;
+			if(currentClipAmmo>maxClipAmmo){
+				currentClipAmmo=maxClipAmmo;
+					
+			}
 	return true;
+
+	}
+	if(currentClipAmmo<maxClipAmmo){
+		
+		currentClipAmmo+=m;
+		if(currentClipAmmo>maxClipAmmo){
+				currentClipAmmo=maxClipAmmo;
+				return true;
+		}
+	}
+
 }
 int Weapon::getCurrentClipAmmo(){
 return currentClipAmmo;
@@ -49,6 +83,17 @@ return currentClipAmmo;
 int Weapon::getCurrentAmmo(){
 return currentAmmo;
 
+}
+
+
+int Weapon::getMaxAmmo(){
+return maxAmmo;
+
+}
+
+int Weapon::getMaxClipAmmo()
+{
+	return maxClipAmmo;
 }
 
 
@@ -63,9 +108,8 @@ int Weapon::getSize(){
 return explosionSize;
 }
 
-void Weapon::handle_events(SDL_Event event,vector<Enemy*> enemies, SDL_Surface * screen, int &score, int &money)
+void Weapon::handle_events(SDL_Event event,vector<Enemy*> enemies, SDL_Surface * screen, int &score, int &money, Crosshairs &crosshairs)
 {
-
 //The mouse offsets
   int x = 0, y = 0;
 int enemyX = 0;
@@ -96,9 +140,14 @@ if(currentClipAmmo>0)
 	if ((x > enemyX) && (x < enemyX + enemyWidth) && (y > enemyY)
 	      && (y < enemyY + enemyHeight))
 	    {
+		if(enemies[i]->isDead()==false)
+		{
+		crosshairs.setImage("crosshairs.png");
+		crosshairs.setCrosshairsGreen(false);
 		enemies[i]->getAttacked(damage,score,money);
 		score+=HIT_BONUS;
 		money+=HIT_BONUS;
+		}
 	    }
 
 	}
@@ -108,18 +157,37 @@ if(currentClipAmmo>0)
   }
 else{
 reloadTime--;
-if(reloadTime<=0){
-currentClipAmmo=maxClipAmmo;
-currentAmmo=currentAmmo-maxClipAmmo;
-reloadTime=20;}
+	if(reloadTime<=0){
+		if(currentAmmo>=maxClipAmmo)
+		{
+			currentClipAmmo=maxClipAmmo;
+			currentAmmo=currentAmmo-maxClipAmmo;
+		}
+		else if (currentAmmo>0)
+		{
+			currentClipAmmo = currentAmmo;
+			currentAmmo = 0;
+		}
+		reloadTime=RELOAD_TIME;
+	}
 }
-}
-/*
-int Weapon::getPrice()
+
+if(crosshairs.isTheCrosshairsGreen()==false)
 {
-	return price;
+
+	crosshairsReloadTime--;
+	//cout<<crosshairsReloadTime<<endl;
+	if(crosshairsReloadTime<=0)
+	{
+		cout<<"got in reload time is 0"<<endl;
+		crosshairs.setImage("greenCrosshairs.png");
+		crosshairs.setCrosshairsGreen(true);
+		crosshairsReloadTime = 30;
+	}
 }
-*/
+}
+
+
 void Weapon::apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip){ //applies new surface
     
 
