029900eac158a18f43f00c9e159ab69699d04c10
Erich Austin Kerekes <ekerekes@student00.cse.nd.edu>
2013-04-24 13:18:34 -0400

    added Pikachu

---
+++ Master/Pikachu.h
@@ -0,0 +1,20 @@
+#ifndef PIKACHU_H
+#define PIKACHU_H
+#include "Enemy.h"
+#include "Sprite.h"
+#include <string>
+
+class Pikachu: public Enemy
+{
+	public:
+		Pikachu(string,int, int, int, int , int, int ,int, int); //constructor
+		 virtual void move(); //move function
+		virtual void show(SDL_Surface*,int);
+		virtual void setClips();
+
+
+	private:
+		SDL_Rect clips[11]; //the number of clips in the Pikachu sprite sheet
+
+};
+#endif
